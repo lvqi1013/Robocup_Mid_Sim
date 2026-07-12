d4583a27618da28e6fd37cd268fe212038c3defd
lvqi <1595642896@qq.com>
2026-07-12 16:48:18 +0800

    updata game ready

--- gameReady.sh
+++ gameReady.sh
@@ -1,3 +1,2 @@
 source install/setup.bash
-export GZ_SIM_SYSTEM_PLUGIN_PATH=/home/lq/code/Robocup_Mid_Sim/install/gz_hello_plugin/lib:$GZ_SIM_SYSTEM_PLUGIN_PATH
 ros2 launch nubot_gazebo load_world.launch.py 
